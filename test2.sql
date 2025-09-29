ced07dd8aebe16e3989be4307577f7ff4d9326b9
Trimi123123 <trimmerovci10@gmail.com>
2025-09-29 10:17:36 -0700

    Update test2.sql

--- test2.sql
+++ test2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2025 at 07:16 PM
+-- Generation Time: Sep 29, 2025 at 07:16 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,21 +24,9 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `products`
 --
 
--- CREATE TABLE `users` (
---   `id` int(255) NOT NULL,
---   `emri` varchar(255) NOT NULL,
---   `username` varchar(255) NOT NULL,
---   `email` varchar(255) NOT NULL,
---   `password` varchar(255) NOT NULL,
---   `confirm_password` varchar(255) NOT NULL,
---   `is_admin` varchar(255) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
 CREATE TABLE `products` (
   `id` int(255) NOT NULL,
   `Products_name` varchar(255) NOT NULL,
@@ -46,11 +34,32 @@ CREATE TABLE `products` (
   `products_quality` varchar(255) NOT NULL,
   `products_rating` int(255) NOT NULL,
   `products_image` varchar(255) NOT NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- INSERT INTO `users` (`id`, `emri`, `username`, `email`, `password`, `confirm_password`, `is_admin`) VALUES
--- (0, 'trimi', 'trimi12', 'trimi@gmail.com', '$2y$10$IxZvan1zeC9ylyB/HhCsS.uM7Vw5/TRc7lyWYDmzqGUjJLPMDEaPa', '$2y$10$guYNHdMuy9kHjuNCLY3W2u/xh1BE9H7uKOggOncT/ITMpfEuIpLJq', '');
--- COMMIT;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `emri` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confirm_password` varchar(255) NOT NULL,
+  `is_admin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `emri`, `username`, `email`, `password`, `confirm_password`, `is_admin`) VALUES
+(0, 'trimi', 'trimi12', 'trimi@gmail.com', '$2y$10$IxZvan1zeC9ylyB/HhCsS.uM7Vw5/TRc7lyWYDmzqGUjJLPMDEaPa', '$2y$10$guYNHdMuy9kHjuNCLY3W2u/xh1BE9H7uKOggOncT/ITMpfEuIpLJq', ''),
+(0, 'trim', 'trimi', 'trimmerovci10@gmail.com', '$2y$10$XDvXXROmBFyPnQixGdF.hukx/wy3szn/.mz49qYpP4YyAOGNfkeJi', '$2y$10$aJ9d0GXUCYyjmY1cIqJTkeiFwMGA.Jh5LNQehPrNpqe.TcwiQl1X2', '');
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
